017218255f4f163bc596ea0e07bb923dcbb50fc4
gargbit <44647279+gargbit@users.noreply.github.com>
2021-01-23 21:37:27 +0100

    Added User class

---
+++ Shared/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBlog.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Author { get; set; }
+        public string Api { get; set; }
+    }
+}
