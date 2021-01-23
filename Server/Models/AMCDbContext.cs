a39288d5ddc155c38004aa7445dee9a082f04287
gargbit <44647279+gargbit@users.noreply.github.com>
2021-01-23 21:47:20 +0100

    Removed unused autogenerated mysql code from AMCDbContext

--- Server/Models/AMCDbContext.cs
+++ Server/Models/AMCDbContext.cs
@@ -20,15 +20,6 @@ namespace BlazorBlog.Server.Models
         public virtual DbSet<Post> Posts { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=blogger;password=password;database=blazorblog", x => x.ServerVersion("10.3.27-mariadb"));
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Post>(entity =>
